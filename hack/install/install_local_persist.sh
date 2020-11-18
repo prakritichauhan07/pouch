1f5ce0cc16aa754a15dd0142a6808c5c4cde6155
prakritichauhan07 <67620689+prakritichauhan07@users.noreply.github.com>
2020-11-18 20:08:24 +0530

    Update install_local_persist.sh

--- hack/install/install_local_persist.sh
+++ hack/install/install_local_persist.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 
 readonly LOCAL_PERSIST_VERSION="1.3.0"
 readonly cmd="local-persist"
+readonly arch=$(uname -m)
 
 # local_persist::check_version checks the command and the version.
 local_persist::check_version() {
@@ -23,11 +24,20 @@ local_persist::install() {
 
   target="/tmp/${cmd}"
   url="https://github.com/CWSpear/local-persist/releases/download"
-  url="${url}/v${LOCAL_PERSIST_VERSION}/local-persist-linux-amd64"
-
-  wget --quiet -O "${target}" "${url}"
-  chmod +x "${target}"
-  mv "${target}" /usr/bin/
+  if [[ "${arch}" == "aarch64" ]]; then
+    git clone https://github.com/MatchbookLab/local-persist/
+    cd local-persist/
+    curl https://glide.sh/get | sh
+    glide install
+    go build -o /usr/bin/local-persist
+    cd ..
+    rm -rf local-persist/
+  else
+    url="${url}/v${LOCAL_PERSIST_VERSION}/local-persist-linux-amd64"
+    wget --quiet -O "${target}" "${url}"
+    chmod +x "${target}"
+    mv "${target}" /usr/bin/
+  fi
 }
 
 main() {
